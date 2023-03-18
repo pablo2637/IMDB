27f58338f13ae5d1c129c703415fae4b57767e09
pablo2637 <pabli2637@gmail.com>
2023-03-18 20:11:21 +0100

    dbConnect

---
+++ helpers/dbConnect.js
@@ -0,0 +1,17 @@
+const moongose = require('mongoose')
+
+const connect = async () => {
+    try {
+        const response = await moongose.connect(process.env.URI_CONNECT);
+        console.log('Connected to the database...');
+        return response;
+    } catch {
+        return {
+            ok: false,
+            msg: 'Connecion failure.',
+            error
+        }
+    }
+}
+
+module.exports = { connect }
