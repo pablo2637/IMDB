ae6c781c09b03101a35cc275ef184d1b5097ac34
pablo2637 <pabli2637@gmail.com>
2023-03-20 12:43:32 +0100

    unificación de fetchs en un único fetchData

--- helpers/fetchData.js
+++ helpers/fetchData.js
@@ -7,11 +7,13 @@ const urlBaseIMDB = 'https://imdb-api.com/API';
 
 const fetchData = async (tipo, data) => {
     const body = data.body;
-    const bodyJS = JSON.stringify(data.body);
-    const id = data.params.id || '';
+    const bodyJSON = JSON.stringify(data.body);
+    const params = data.params || '';
     const query = data.query || '';
     let url = '';
     let options = {};
+
+    console.log(params, query, params, body)
 
     switch (tipo) {
 
@@ -20,37 +22,39 @@ const fetchData = async (tipo, data) => {
             url = `${urlBase}/${urlAPI}/${urlMoviesMongo}`;
             break;
         case 'getMovieInt':
-            url = `${urlBase}/${urlAPI}/${urlMoviesMongo}/${id}`;
+            url = `${urlBase}/${urlAPI}/${urlMoviesMongo}/${params.id}`;
             break;
         case 'postMovieInt':
             url = `${urlBase}/${urlAPI}/${urlMoviesMongo}`;
             options = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                bodyJS
+                body: bodyJSON
             }
             break;
         case 'putMovieInt':
-            url = `${urlBase}/${urlAPI}/${urlMoviesMongo}/${id}`;
+            url = `${urlBase}/${urlAPI}/${urlMoviesMongo}/${params.id}`;
             options = {
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
-                bodyJS
+                body: bodyJSON
             }
             break;
-        case 'deleteMovieExt':
-            url = `${urlBase}/${urlAPI}/${urlMoviesMongo}/${id}`;
+        case 'deleteMovieInt':
+            url = `${urlBase}/${urlAPI}/${urlMoviesMongo}/${params.id}`;
             options = { method: 'DELETE' }
             break;
 
 
         //Api externa imdb **************************************************
         case 'getMoviesExt':
-            url = `${urlBaseIMDB}/AdvancedSearch/${urlApiKeyIMDB}?title=${body}`; //pendiente de verificar body
+            url = `${urlBaseIMDB}/AdvancedSearch/${urlApiKeyIMDB}?title=${params.title}`; //pendiente de verificar body
             break;
-        case 'getMovieExt':
-            url = `${urlBaseIMDB}/Title/${urlApiKeyIMDB}/${id}`; //pendiente de verificar id
+        case 'getMovieExt':            
+            url = `${urlBaseIMDB}/Title/${urlApiKeyIMDB}/${params.id}`; //pendiente de verificar id
             break;
+
+
     }
 
 
