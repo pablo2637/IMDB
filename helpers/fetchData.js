896b8a12c65513fb53547df910d2afe1d98ecc2f
pablo2637 <pabli2637@gmail.com>
2023-03-20 09:06:14 +0100

    modificación fetchData para igualarla a fetchMovies y unificar

--- helpers/fetchData.js
+++ helpers/fetchData.js
@@ -1,45 +1,79 @@
 const urlBase = process.env.URL_BASE;
 const urlAPI = 'api';
+const urlApiKeyIMDB = process.env.API_IMDB;
 const urlMoviesMongo = 'movies/mongo';
-const urlMoviesIMDB = '';
+// const urlMoviesIMDB = 'movies/imdb';
+const urlBaseIMDB = 'https://imdb-api.com/API';
 
-const fetchData = (tipo, data) => {
-    const body = JSON.stringify(data.body);
+const fetchData = async (tipo, data) => {
+    const body = data.body;
+    const bodyJS = JSON.stringify(data.body);
     const id = data.params.id || '';
     const query = data.query || '';
     let url = '';
     let options = {};
 
     switch (tipo) {
-        case 'getMovies':
+
+        //API interna mongo **************************************************
+        case 'getMoviesInt':
             url = `${urlBase}/${urlAPI}/${urlMoviesMongo}`;
             break;
-        case 'getMovie':
+        case 'getMovieInt':
             url = `${urlBase}/${urlAPI}/${urlMoviesMongo}/${id}`;
             break;
-        case 'postMovie':
+        case 'postMovieInt':
             url = `${urlBase}/${urlAPI}/${urlMoviesMongo}`;
             options = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body
+                bodyJS
             }
             break;
-        case 'putMovie':
+        case 'putMovieInt':
             url = `${urlBase}/${urlAPI}/${urlMoviesMongo}/${id}`;
             options = {
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
-                body
+                bodyJS
             }
             break;
-        case 'deleteMovie':
+        case 'deleteMovieExt':
             url = `${urlBase}/${urlAPI}/${urlMoviesMongo}/${id}`;
             options = { method: 'DELETE' }
             break;
+
+
+        //Api externa imdb **************************************************
+        case 'getMoviesExt':
+            url = `${urlBaseIMDB}/AdvancedSearch/${urlApiKeyIMDB}?title=${body}`; //pendiente de verificar body
+            break;
+        case 'getMovieExt':
+            url = `${urlBaseIMDB}/Title/${urlApiKeyIMDB}/${id}`; //pendiente de verificar id
+            break;
     }
 
-    return { url, options }
+
+    try {
+        const request = await fetch(url, options);
+        const response = await request.json();
+        if (!response) return {
+            ok: false,
+            msg: 'Error fetchData',
+            response
+        }
+
+        return {
+            ok: true,
+            data: response
+        }
+    } catch (e) {
+        console.log('error', e)
+        return {
+            ok: false,
+            error: e
+        }
+    }
 }
 
 module.exports = { fetchData }
