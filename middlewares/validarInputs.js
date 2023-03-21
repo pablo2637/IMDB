85e4b6a4808aee4328fbd6b6b88e2ae543a8b870
Marcos <perezocanamarcos@gmail.com>
2023-03-21 09:05:54 +0100

    validate inputs a la espera de mas datos y ver SQL

---
+++ middlewares/validarInputs.js
@@ -0,0 +1,16 @@
+const  {validationResult}= require ("express-validator");
+
+const validarInputs = (req,res,next)=>{
+    const fallos =validationResult(req);
+    if(!fallos.isEmpty()){
+        return res.status(400).json({
+            ok:false,
+            errores: fallos.mapped()
+        })
+    }
+    next()
+}
+
+module.exports={
+    validarInputs,
+}
