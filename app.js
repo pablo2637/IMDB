28229fa98988c23b7427b6be9e02ea3f6692de0b
pablo2637 <pabli2637@gmail.com>
2023-03-21 19:58:21 +0100

    test auth0

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const cors = require('cors');
 require('dotenv').config();
 const { connect } = require('./helpers/dbConnect')
 
+const { auth } = require('express-openid-connect'); //Auth0
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.A0_SECRET_KEY,
+    baseURL: process.env.URL_BASE,
+    clientID: process.env.A0_CLIENT_ID,
+    issuerBaseURL: process.env.A0_ISSUER_BASE_URL
+};
+
 const app = express();                              //Servidor
 const port = process.env.PORT;
 
@@ -15,10 +25,13 @@ app.set('views', __dirname + '/views');
 app.use(express.urlencoded({ extended: false }))    // Parse application/x-www-form-urlencoded
 app.use(express.json())                             // Parse application/json
 
+app.use(auth(config));                              //Auth0 config
+
 //Conexión
 connect();
 
 //Rutas
+app.use('/', require('./routers/routerFront')); //test  
 app.use('/api/movies/mongo', require('./routers/routerApiMoviesAdmin'));
 app.use('/api/movies/imdb', require('./routers/routerAPIExterna')); // ruta API externa (IMDb)
 
